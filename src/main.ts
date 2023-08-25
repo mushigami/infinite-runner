8b49cf0aac893c2c49148a35db71bf4f5d8d9c44
mushigami <asametcelebi@hotmail.com>
2023-08-25 22:30:25 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import Phaser from 'phaser'
+
+//import HelloWorldScene from './scenes/HelloWorldScene'
+import Game from './scenes/Game'
+
+const config: Phaser.Types.Core.GameConfig = {
+	type: Phaser.AUTO,
+	parent: 'app',
+	width: 800,
+	height: 640,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 },
+		},
+	},
+	scene: [Game],
+}
+
+export default new Phaser.Game(config)
